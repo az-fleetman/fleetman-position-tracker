b304ef984756220678be91be1913c0e403eee4b4
Andrea Zuccollo <andrea@admins-Mac-mini.local>
2020-05-23 22:42:39 +0200

    changed az

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -33,7 +33,7 @@ public class MessageProcessor {
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
 				                          .withLng(new BigDecimal(incomingMessage.get("long")))
 				                          .withTimestamp(convertedDatestamp)
-				                          .withSpeed(45.8)
+				                          .withSpeed(new BigDecimal("45.8"))
 				                          .build();
 				                          
 		data.updatePosition(newReport);
